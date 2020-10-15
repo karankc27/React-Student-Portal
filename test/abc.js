535d47aaacf869947c1228698755b83bf85948b4
mahakarora0921 <mahakarora0921@gmail.com>
2020-10-15 20:17:42 +0530

    Added content to abc

--- test/abc.js
+++ test/abc.js
@@ -1,1 +1,11 @@
 console.log("hello")
+// Abse ye command run krna update lene k liye
+// git fetch
+// git pull origin master
+
+
+// Jo bhi changes kre:
+// git checkout mahak
+// git add .
+// git commmit -m "Kya change kra wo"
+// git push --set-upstream origin mahak
