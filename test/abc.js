297f27a37696a5e58fa7101b5303b9a1210fe77e
mahakarora0921 <mahakarora0921@gmail.com>
2020-10-15 20:27:30 +0530

    Created blank file

--- test/abc.js
+++ test/abc.js
@@ -3,6 +3,9 @@ console.log("hello")
 // git fetch
 // git pull origin master
 
+// 3-4 line tab jab mai kuch change kru n tujhe mera update code lena ho
+// 10-end jab tu kuch update kre n mai maangu.. jab bolu ya dekhna ho mujhe bs tabhi krna bhale 
+
 
 // Jo bhi changes kre:
 // git checkout mahak
